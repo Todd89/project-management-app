f0b429737d893b5a687e6bd1cb5035b8451f8970
Aleksandr Golamonov <alex-89fish@tut.by>
2022-05-14 11:53:52 +0300

    feat: realize boundary-error

--- react-app/src/components/pages/WelcomeRoute/WelcomeRoute.tsx
+++ react-app/src/components/pages/WelcomeRoute/WelcomeRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
